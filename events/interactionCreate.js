7b2482e201335f7c54ab9dc0e1e15c82ebc86508
feyzulllah <valorantplay12222@gmail.com>
2026-03-01 13:56:46 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,16 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction, client) {
+        if (!interaction.isChatInputCommand()) return;
 
+        const command = client.commands.get(interaction.commandName);
+        if (!command) return;
+
+        try {
+            await command.execute(interaction, null, client);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'Komut çalıştırılırken hata oluştu!', ephemeral: true });
+        }
+    }
+};
