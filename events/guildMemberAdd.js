8394e51a3d88e257cc5b5837c55133d530c5ab56
feyzulllah <valorantplay12222@gmail.com>
2026-03-01 15:07:44 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,14 +3,18 @@ const config = require('../config.json');
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
-        // Otorol Verme (ID: 1477624928856571914)
-        const rol = member.guild.roles.cache.get(config.otoRol);
-        if (rol) await member.roles.add(rol).catch(() => console.log("Rol yetkim yetersiz."));
+        // 1. Rolü Verme
+        const rol1 = member.guild.roles.cache.get(config.otoRol1);
+        if (rol1) await member.roles.add(rol1).catch(() => console.log("1. Rol yetkim yetersiz."));
 
-        // Hoş Geldin Mesajı (ID: 1477626275269972000)
+        // 2. Rolü Verme
+        const rol2 = member.guild.roles.cache.get(config.otoRol2);
+        if (rol2) await member.roles.add(rol2).catch(() => console.log("2. Rol yetkim yetersiz."));
+
+        // Hoş Geldin Mesajı
         const kanal = member.guild.channels.cache.get(config.hgKanal);
         if (kanal) {
-            kanal.send({ content: `📥 **${member.user.tag}** sunucumuza katıldı! Sunucumuz artık **${member.guild.memberCount}** kişi.` });
+            kanal.send({ content: `📥 **${member.user.tag}** sunucumuza katıldı! Seninle birlikte **${member.guild.memberCount}** kişiyiz.` });
         }
     },
 };
