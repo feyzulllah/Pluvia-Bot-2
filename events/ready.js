331707e74689f104bd17dac2c26d007d277ac205
feyzulllah <valorantplay12222@gmail.com>
2026-03-14 01:25:10 +0300

    Delete events directory

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client) {
-        console.log(`✅ ${client.user.tag} Gelişmiş Sistemlerle Aktif!`);
-        client.user.setActivity('!yardım | Pluvia V2');
-    },
-};
